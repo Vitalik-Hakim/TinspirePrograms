a0f5d32fc77d8949ea20063a50491e6995d83533
Vitalik-Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2021-10-13 15:59:27 +0000

    Update hello_world.asm

--- asm/hello_world.asm
+++ asm/hello_world.asm
@@ -2,3 +2,17 @@
 ;
 ; Author: Vitalik Hakim
 ; Date: 13-oct-2021
+global _start
+
+section .text:
+
+_start:
+   mov eax, 0x4          ;use the write syscall
+   mov ebx, 1            ; use the stdout as the fd
+   mov ecx, message      ; use the message as the buffer
+   mov edx, message_length ; and supply the length
+   int 0x80               ; invoke the syscall  
+    
+    ; Now exit
+
+   
